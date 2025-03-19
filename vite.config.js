75ec6b44f783fa6bceb1da49ced3378bf705467c
Kateryna-Katya <katyaukr2014@gmail.com>
2025-03-19 13:38:21 +0200

    fix import

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
-// vite.config.js
 import { defineConfig } from 'vite';
+import glob from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig({
-  optimizeDeps: {
-    include: ['swiper'],
-  },
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    build: {
+      sourcemap: true,
+
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
+        },
+      },
+      outDir: '../dist',
+    },
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  };
 });
