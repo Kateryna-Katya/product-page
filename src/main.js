e6c0513766fa9de90331d620ac1383384cdd7bbf
Kateryna-Katya <katyaukr2014@gmail.com>
2025-03-19 17:42:39 +0200

    delete import

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+
 
 document.addEventListener('DOMContentLoaded', function () {
     let logosSwiper;
